79c63c671c303b87c21856b2d6797daa4199cbf4
faileon <pt4cekt@gmail.com>
2025-01-07 10:15:09 +0100

    feat: update packages

--- lua/configs/tiny-inline-diagnostic.lua
+++ lua/configs/tiny-inline-diagnostic.lua
@@ -1,1 +1,5 @@
-require("tiny-inline-diagnostic").setup {}
+require("tiny-inline-diagnostic").setup {
+  options = {
+    show_source = true,
+  },
+}
--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -134,6 +134,18 @@ local plugins = {
       require "configs.noice"
     end,
   },
+  -- {
+  --   "benlubas/molten-nvim",
+  --   event = "VeryLazy",
+  --   version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+  --   dependencies = { "3rd/image.nvim" },
+  --   build = ":UpdateRemotePlugins",
+  --   init = function()
+  --     -- these are examples, not defaults. Please see the readme
+  --     vim.g.molten_image_provider = "image.nvim"
+  --     vim.g.molten_output_win_max_height = 20
+  --   end,
+  -- },
   {
     "3rd/image.nvim",
     event = "VeryLazy",
